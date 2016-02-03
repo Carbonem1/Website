92c5405dc73243a967e5ba9be40cd770755b7925
Michael Carbone <carbonem1@wit.edu>
2016-02-03 16:35:52 -0500

    Fixed display for home page on laptop. Added curation project and contact info.

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
 function aboutSlide()
-{	
+{
 	var img1 = document.getElementById("about-img");
 	var img2 = document.getElementById("resume-img");
 	var img3 = document.getElementById("projects-img");
 	var img4 = document.getElementById("contact-img");
-	
-	img2.style.transition = "opacity .75s ease-out 0s";	
+
+	img2.style.transition = "opacity .75s ease-out 0s";
 	img2.style.opacity = 0;
 	img3.style.transition = "opacity .75s ease-out 0s";
 	img3.style.opacity = 0;
@@ -13,9 +13,9 @@ function aboutSlide()
 	img4.style.opacity = 0;
 
 	img1.style.transition = "left .75s ease-out 0s";
-	img1.style.left = "480px";
+	img1.style.left = "326px";
 	img1.style.top = "166px";
-	
+
 	setTimeout(continueAboutExecution, 1000);
 }
 
@@ -25,13 +25,13 @@ function continueAboutExecution()
 }
 
 function resumeSlide()
-{	
+{
 	var img1 = document.getElementById("about-img");
 	var img2 = document.getElementById("resume-img");
 	var img3 = document.getElementById("projects-img");
 	var img4 = document.getElementById("contact-img");
-	
-	img1.style.transition = "opacity .75s ease-out 0s";	
+
+	img1.style.transition = "opacity .75s ease-out 0s";
 	img1.style.opacity = 0;
 	img3.style.transition = "opacity .75s ease-out 0s";
 	img3.style.opacity = 0;
@@ -39,9 +39,9 @@ function resumeSlide()
 	img4.style.opacity = 0;
 
 	img2.style.transition = "left .75s ease-out 0s";
-	img2.style.left = "480px";
+	img2.style.left = "322px";
 	img2.style.top = "166px";
-	
+
 	setTimeout(continueResumeExecution, 1000);
 }
 
@@ -51,13 +51,13 @@ function continueResumeExecution()
 }
 
 function projectsSlide()
-{	
+{
 	var img1 = document.getElementById("about-img");
 	var img2 = document.getElementById("resume-img");
 	var img3 = document.getElementById("projects-img");
 	var img4 = document.getElementById("contact-img");
-	
-	img1.style.transition = "opacity .75s ease-out 0s";	
+
+	img1.style.transition = "opacity .75s ease-out 0s";
 	img1.style.opacity = 0;
 	img2.style.transition = "opacity .75s ease-out 0s";
 	img2.style.opacity = 0;
@@ -65,9 +65,9 @@ function projectsSlide()
 	img4.style.opacity = 0;
 
 	img3.style.transition = "left .75s ease-out 0s";
-	img3.style.left = "480px";
+	img3.style.left = "322px";
 	img3.style.top = "166px";
-	
+
 	setTimeout(continueProjectsExecution, 1000);
 }
 
@@ -77,23 +77,23 @@ function continueProjectsExecution()
 }
 
 function contactSlide()
-{	
+{
 	var img1 = document.getElementById("about-img");
 	var img2 = document.getElementById("resume-img");
 	var img3 = document.getElementById("projects-img");
 	var img4 = document.getElementById("contact-img");
-	
-	img1.style.transition = "opacity .75s ease-out 0s";	
+
+	img1.style.transition = "opacity .75s ease-out 0s";
 	img1.style.opacity = 0;
 	img2.style.transition = "opacity .75s ease-out 0s";
 	img2.style.opacity = 0;
 	img3.style.transition = "opacity .75s ease-out 0s";
 	img3.style.opacity = 0;
-	
+
 	img4.style.transition = "left .75s ease-out 0s";
-	img4.style.left = "480px";
+	img4.style.left = "322px";
 	img4.style.top = "166px";
-	
+
 	setTimeout(continueContactExecution, 1000);
 }
 
